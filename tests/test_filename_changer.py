fa64b0e8e80ad888399ea7f232f901e98fe67e29
SadLaboka <jitubuh@gmail.com>
2022-05-02 16:29:54 +0300

    add tests

--- tests/test_filename_changer.py
+++ tests/test_filename_changer.py
@@ -1,7 +1,8 @@
 from page_loader.filename_changer import (
     add_extension,
     delete_extension,
-    get_name
+    get_name,
+    remove_trailing_slash
 )
 
 
@@ -12,10 +13,16 @@ def test_add_extension():
     assert add_extension(name, extension) == result
 
 
-def test_get_filename():
+def test_get_name():
     test_link = 'https://ru.hexlet.io/courses'
     result = 'ru-hexlet-io-courses'
     assert get_name(test_link) == result
+
+
+def test_get_name_without_scheme():
+    test_path = '/files/static/123.png'
+    result = '-files-static-123'
+    assert get_name(test_path) == result
 
 
 def test_delete_extension():
@@ -27,3 +34,15 @@ def test_delete_extension():
 def test_delete_extension_without_extension():
     link = 'http://test.com/25/31213/dva'
     assert delete_extension(link) == link
+
+
+def test_delete_extension_with_other():
+    link = 'http://test.com/jnlp'
+    assert delete_extension(link) == link
+
+
+def test_remove_trailing_slash():
+    link = 'http://test.com/'
+    result = 'http://test.com'
+    assert remove_trailing_slash(link) == result
+    assert remove_trailing_slash(result) == result
